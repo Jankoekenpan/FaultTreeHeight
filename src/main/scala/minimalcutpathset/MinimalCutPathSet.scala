b15d9ec19f3f6878125f31493645b6a564d6227b
Jan Boerman <JanBoerman95@gmail.com>
2024-10-15 23:23:24 +0200

    Implement minimal cut sets and minimal path sets calculations.

--- src/main/scala/minimalcutpathset/MinimalCutPathSet.scala
+++ src/main/scala/minimalcutpathset/MinimalCutPathSet.scala
@@ -1,17 +1,37 @@
 package minimalcutpathset
 
+import minimalcutpathset.TreeNode.Combination
+
 import java.util.random.RandomGenerator
+import scala.jdk.CollectionConverters.given
+import scala.util.boundary
+import scala.util.boundary.break
+
+enum Gate:
+    case And
+    case Or
+
+enum TreeNode(id: Event):
+    case Combination(id: Event, gate: Gate, children: Set[Event]) extends TreeNode(id)
+    case BasicEvent(id: Event, probability: Probability) extends TreeNode(id)
+
+case class FaultTree(topEvent: Event, events: Map[Event, TreeNode]):
+    def node(id: Event): TreeNode = events(id)
+    def topNode = node(topEvent)
 
 type Event = Int
 type Probability = Double
 
 enum Decision:
-    case Left   // 0
-    case Right  // 1
+    case Zero   // 0
+    case One    // 1
 type Path = List[Decision]
 
-type CutSets = Seq[Set[Event]]
-type PathSets = Seq[Set[Event]]
+type CutSet = Set[Event]
+type CutSets = Set[CutSet]
+
+type PathSet = Set[Event]
+type PathSets = Set[PathSet]
 
 // TODO could probably use Trie data-structure for better efficiency.
 
@@ -34,8 +54,8 @@ def nonZero(height: Height): Boolean = height match
     case f: Function0[Double] => true
 
 def other(decision: Decision): Decision = decision match
-    case Decision.Left => Decision.Right
-    case Decision.Right => Decision.Left
+    case Decision.Zero => Decision.One
+    case Decision.One => Decision.Zero
 
 def approximate1(minimalCutsets: CutSets, minimalPathsets: PathSets, basicEvents: Seq[Probability], random: RandomGenerator): (Etas, Double) = {
     val n = basicEvents.size
@@ -52,13 +72,13 @@ def approximate1(minimalCutsets: CutSets, minimalPathsets: PathSets, basicEvents
     etas.update(List(), a)
 
     val probA: Probability = basicEvents(a)
-    val hNil: Function0[Double] = new CachedFunction0(() => 1D + (1D - probA) * asDouble(heights(List(Decision.Left))) + probA * asDouble(heights(List(Decision.Right))))
+    val hNil: Function0[Double] = new CachedFunction0(() => 1D + (1D - probA) * asDouble(heights(List(Decision.Zero))) + probA * asDouble(heights(List(Decision.One))))
     heights.update(List(), hNil)
 
     for i <- 0 to (n - 1) do
         if X(i).nonEmpty then
             for x <- X(i) do
-                for j <- Seq[Decision](Decision.Right, Decision.Left) do
+                for j <- Seq[Decision](Decision.One, Decision.Zero) do
                     val s: Path = j :: x
                     val t: Path = x.dropWhile(_ == other(j))
 
@@ -67,7 +87,7 @@ def approximate1(minimalCutsets: CutSets, minimalPathsets: PathSets, basicEvents
                     vertices.put(s, Vs)
 
                     // TODO can we compute this more efficiently?
-                    val Bs: Seq[Event] = if j == Decision.Right then
+                    val Bs: Set[Event] = if j == Decision.One then
                         // { x | x <- cutset, cutset <- cutsets, Vs subSet cutset, x notIn Vs}
                         for { cutSet <- minimalCutsets; if Vs.subsetOf(cutSet); x <- cutSet; if !Vs.contains(x) } yield x
                     else
@@ -75,7 +95,7 @@ def approximate1(minimalCutsets: CutSets, minimalPathsets: PathSets, basicEvents
                         for { pathSet <- minimalPathsets; if Vs.subsetOf(pathSet); x <- pathSet; if !Vs.contains(x) } yield x
 
                     val (etaS: Eta, heightS: Height) = if Bs.nonEmpty then
-                        val b = if j == Decision.Right then
+                        val b = if j == Decision.One then
                             Bs.maxBy(b => basicEvents(b))
                         else
                             Bs.minBy(b => basicEvents(b))
@@ -85,8 +105,8 @@ def approximate1(minimalCutsets: CutSets, minimalPathsets: PathSets, basicEvents
                             case None => Some(Set(s))
                         }
                         (b: Eta, CachedFunction0(() => 1 +
-                            (1 - probB) * asDouble(heights(Decision.Left :: s)) +
-                            probB * asDouble(heights(Decision.Right :: s))))
+                            (1 - probB) * asDouble(heights(Decision.Zero :: s)) +
+                            probB * asDouble(heights(Decision.One :: s))))
                     else
                         (j: Eta, 0: Height)
 
@@ -119,12 +139,108 @@ object Unset
 @main
 def main(): Unit = {
 
-    val cutsets = Seq(Set(0), Set(1, 2), Set(1, 3))
-    val pathsets = Seq(Set(0, 1), Set(0, 2, 3))
+    val cutsets = Set(Set(0), Set(1, 2), Set(1, 3))
+    val pathsets = Set(Set(0, 1), Set(0, 2, 3))
     val probabilities = IndexedSeq(2D/3D, 1D/4D, 1D/3D, 1D/2D)
 
     val (etas, hNil) = approximate1(cutsets, pathsets, probabilities, new java.util.Random())
 
     println(etas)
     println(hNil)
+
+    val exampleTree = FaultTree(0, Map(
+        0 -> TreeNode.Combination(0, Gate.Or, Set(1, 2)),
+        1 -> TreeNode.BasicEvent(1, 0D),    // A
+        2 -> TreeNode.Combination(2, Gate.And, Set(3, 4)),
+        3 -> TreeNode.BasicEvent(3, 0D),    // B
+        4 -> TreeNode.Combination(4, Gate.Or, Set(5, 6)),
+        5 -> TreeNode.BasicEvent(5, 0D),    // C
+        6 -> TreeNode.BasicEvent(6, 0D),    // D
+    ))
+
+    println(minimalCutSets(exampleTree))
+    println(minimalPathSets(exampleTree))
 }
+
+def minimalCutSets(faultTree: FaultTree): CutSets = {
+    val events = faultTree.events
+
+    def minimalCutSet(event: TreeNode): CutSets = event match {
+        case TreeNode.BasicEvent(id, probability) => Set(Set(id))
+        case TreeNode.Combination(id, Gate.And, children) =>
+            val childCutSets: Set[CutSets] = for {
+                child <- children
+                childNode = faultTree.node(child)
+                childCutSet = minimalCutSet(childNode)
+            } yield childCutSet
+            carthesianUnion(childCutSets)
+        case TreeNode.Combination(id, Gate.Or, children) =>
+            val childCutSets: Set[CutSets] = for {
+                child <- children
+                childNode = faultTree.node(child)
+                childCutSet = minimalCutSet(childNode)
+            } yield childCutSet
+            removeSupersets(childCutSets.flatten)
+    }
+
+    minimalCutSet(faultTree.topNode)
+}
+
+def minimalPathSets(faultTree: FaultTree): PathSets = {
+    val events = faultTree.events
+
+    def minimalPathSet(event: TreeNode): PathSets = event match {
+        case TreeNode.BasicEvent(id, probability) => Set(Set(id))
+        case TreeNode.Combination(id, Gate.Or, children) =>
+            val childPathSets: Set[PathSets] = for {
+                child <- children
+                childNode = faultTree.node(child)
+                childPathSet = minimalPathSet(childNode)
+            } yield childPathSet
+            carthesianUnion(childPathSets)
+        case TreeNode.Combination(id, Gate.And, children) =>
+            val childPathSets: Set[PathSets] = for {
+                child <- children
+                childNode = faultTree.node(child)
+                childPathSet = minimalPathSet(childNode)
+            } yield childPathSet
+            removeSupersets(childPathSets.flatten)
+    }
+
+    minimalPathSet(faultTree.topNode)
+}
+
+def carthesianUnion(cutSetss: Set[CutSets]): CutSets = {
+    val cutSets: CutSets = for {
+        cutSetsX <- cutSetss
+        cutSetsY <- cutSetss
+        if !(cutSetsX eq cutSetsY)
+        cutSetX <- cutSetsX
+        cutSetY <- cutSetsY
+    } yield cutSetX ++ cutSetY
+    removeSupersets(cutSets)
+}
+
+def removeSupersets(cutSets: CutSets): CutSets = {
+    val result = new java.util.HashSet[CutSet]()
+
+    boundary {
+        val cutSetIterator = cutSets.iterator
+        while (cutSetIterator.hasNext) {
+            val cutSet = cutSetIterator.next()
+            val resIterator = result.iterator
+            while (resIterator.hasNext) {
+                val resSet = resIterator.next()
+                if (resSet.subsetOf(cutSet)) {
+                    break() // don't add cutSet to result
+                } else if (cutSet.subsetOf(resSet)) {
+                    resIterator.remove()
+                }
+            }
+            result.add(cutSet)
+        }
+    }
+
+    result.asScala.toSet
+}
+
