b86374de21667cffc114a107a6bce9dd4b13ada4
Jan Boerman <JanBoerman95@gmail.com>
2024-11-05 22:36:25 +0100

    Implement elimination algorithm.

--- src/main/scala/decisiontree/BinaryDecisionTree.scala
+++ src/main/scala/decisiontree/BinaryDecisionTree.scala
@@ -1,7 +1,5 @@
 package decisiontree
 
-import decisiontree.Path.ConsLeft
-
 import scala.annotation.tailrec
 
 type Event = Int
@@ -11,22 +9,67 @@ enum BinaryDecisionTree:
     case One
     case NonLeaf(id: Event, left: BinaryDecisionTree, right: BinaryDecisionTree)
 
+def isLeafNode(binaryDecisionTree: BinaryDecisionTree): Boolean = binaryDecisionTree match
+    case BinaryDecisionTree.Zero | BinaryDecisionTree.One => true
+    case _ => false
+
 enum Path:
     case Leaf(id: Event)
     case ConsLeft(id: Event, tail: Path)
     case ConsRight(id: Event, tail: Path)
 
+    def head: Event = this match
+        case Leaf(id) => id
+        case ConsLeft(id, _) => id
+        case ConsRight(id, _) => id
+
+    // TODO can cache results if computation becomes slow. Can use something like CachedFunction0.
+    def last: Event = this match
+        case Leaf(id) => id
+        case ConsLeft(_, tail) => tail.last
+        case ConsRight(_, tail) => tail.last
+
+//    def init: Path = this match
+//        case Leaf(_) => throw new NoSuchElementException("init of Leaf")
+//        case ConsLeft(id, Leaf(_)) => Leaf(id)
+//        case ConsLeft(id, tail) => ConsLeft(id, tail.init)
+//        case ConsRight(id, Leaf(_)) => Leaf(id)
+//        case ConsRight(id, tail) => ConsRight(id, tail.init)
+
+    def upTo(event: Event): Path = this match
+        case p@Leaf(id) => if event == id then p else throw new NoSuchElementException("Not found: " + event)
+        case ConsLeft(id, _) if event == id => Leaf(id)
+        case ConsLeft(id, tail) => ConsLeft(id, tail.upTo(event))
+        case ConsRight(id, _) if event == id => Leaf(id)
+        case ConsRight(id, tail) => ConsRight(id, tail.upTo(event))
+
+    def from(event: Event): Path = this match
+        case p@Leaf(id) => if event == id then p else throw new NoSuchElementException("Not found: " + event)
+        case p@ConsLeft(id, tail) => if event == id then p else tail.from(event)
+        case p@ConsRight(id, tail) => if event == id then p else tail.from(event)
+
+    def addLastLeft(event: Event): Path = this match
+        case Leaf(id) => ConsLeft(id, Leaf(event))
+        case ConsLeft(id, tail) => ConsLeft(id, tail.addLastLeft(event))
+        case ConsRight(id, tail) => ConsRight(id, tail.addLastLeft(event))
+
+    def addLastRight(event: Event): Path = this match
+        case Leaf(id) => ConsRight(id, Leaf(event))
+        case ConsLeft(id, tail) => ConsLeft(id, tail.addLastRight(event))
+        case ConsRight(id, tail) => ConsRight(id, tail.addLastRight(event))
+
 //enum Direction:
 //    case Left
 //    case Right
 
-def getPaths(bdt: BinaryDecisionTree): Seq[Path] = bdt match
-    case BinaryDecisionTree.Zero => Seq()
-    case BinaryDecisionTree.One => Seq()
+def getPaths(bdt: BinaryDecisionTree): LazyList[Path] = bdt match
+    case BinaryDecisionTree.Zero => LazyList.empty
+    case BinaryDecisionTree.One => LazyList.empty
     case BinaryDecisionTree.NonLeaf(id, l, r) =>
-        val pathsLeft = getPaths(l)
-        val pathsRight = getPaths(r)
-        pathsLeft.map(pl => Path.ConsLeft(id, pl)) ++ pathsRight.map(pr => Path.ConsRight(id, pr))
+        if isLeafNode(l) && isLeafNode(r) then
+            LazyList(Path.Leaf(id))
+        else
+            getPaths(l).map(pl => Path.ConsLeft(id, pl)) ++ getPaths(r).map(pr => Path.ConsRight(id, pr))
 
 @tailrec
 def isOnlyLeft(path: Path): Boolean = path match
@@ -40,27 +83,6 @@ def isOnlyRight(path: Path): Boolean = path match
     case Path.ConsLeft(_, _) => false
     case Path.ConsRight(_, tail) => isOnlyRight(tail)
 
-//def collectDirections(path: Path): Seq[Direction] = path match
-//    case Path.Leaf(_) => Nil
-//    case Path.ConsLeft(_, tail) => Direction.Left +: collectDirections(tail)
-//    case Path.ConsRight(_, tail) => Direction.Right +: collectDirections(tail)
-//
-//def isOnlyLeft(directions: Seq[Direction]): Boolean = directions.forall(_ == Direction.Left)
-//def isOnlyRight(directions: Seq[Direction]): Boolean = directions.forall(_ == Direction.Right)
-
-def replaceSubtree(tree: BinaryDecisionTree, eventToReplace: Event, replacement: BinaryDecisionTree): BinaryDecisionTree = tree match {
-    case BinaryDecisionTree.Zero => BinaryDecisionTree.Zero
-    case BinaryDecisionTree.One => BinaryDecisionTree.One
-    case BinaryDecisionTree.NonLeaf(id, left, right) =>
-        if id == eventToReplace then
-            replacement
-        else
-            BinaryDecisionTree.NonLeaf(id,
-                replaceSubtree(left, eventToReplace, replacement),
-                replaceSubtree(right, eventToReplace, replacement))
-}
-
-//TODO return the slice between the repeated vertices directly (as Path).
 def repeatedVertex(path: Path): Option[Event] = {
     @tailrec
     def repeatedVertex(path: Path, seen: Set[Event]): Option[Event] = path match {
@@ -72,18 +94,100 @@ def repeatedVertex(path: Path): Option[Event] = {
     repeatedVertex(path, Set())
 }
 
-def eliminate(bdt: BinaryDecisionTree): BinaryDecisionTree = {
-    for (path <- getPaths(bdt)) {
-        repeatedVertex(path) match {
-            case Some(repeatedVertexId) =>
-                //TODO
-                ???
-            case None =>
-                //TODO
-                ???
-        }
-    }
+@tailrec
+def traverse(binaryDecisionTree: BinaryDecisionTree, path: Path): BinaryDecisionTree = {
+    assert:
+        binaryDecisionTree match
+            case BinaryDecisionTree.NonLeaf(treeNodeId, _, _) => treeNodeId == path.head
+            case _ => false
 
-    //TODO
-    ???
+    val BinaryDecisionTree.NonLeaf(_, left, right) = binaryDecisionTree: @unchecked
+
+    path match
+        case Path.Leaf(_) => binaryDecisionTree
+        case Path.ConsLeft(_, tail) => traverse(left, tail)
+        case Path.ConsRight(_, tail) => traverse(right, tail)
+}
+
+def replaceViaPath(top: BinaryDecisionTree, path: Path, replacement: BinaryDecisionTree): BinaryDecisionTree = top match {
+    case BinaryDecisionTree.Zero | BinaryDecisionTree.One => top
+    case BinaryDecisionTree.NonLeaf(id, left, right) =>
+        assert:
+            id == path.head
+
+        path match
+            case Path.Leaf(_) => replacement
+            case Path.ConsLeft(_, tail) => BinaryDecisionTree.NonLeaf(id, replaceViaPath(left, tail, replacement), right)
+            case Path.ConsRight(_, tail) => BinaryDecisionTree.NonLeaf(id, left, replaceViaPath(right, tail, replacement))
+}
+
+def replaceAfterPathLeft(top: BinaryDecisionTree, path: Path, replacement: BinaryDecisionTree): BinaryDecisionTree = top match {
+    case BinaryDecisionTree.Zero | BinaryDecisionTree.One => replacement
+    case BinaryDecisionTree.NonLeaf(id, left, right) =>
+        assert:
+            id == path.head
+
+        path match
+            case Path.Leaf(_) => BinaryDecisionTree.NonLeaf(id, replacement, right)
+            case Path.ConsLeft(_, tail) => BinaryDecisionTree.NonLeaf(id, replaceAfterPathLeft(left, tail, replacement), right)
+            case Path.ConsRight(_, tail) => BinaryDecisionTree.NonLeaf(id, left, replaceAfterPathLeft(right, tail, replacement))
+}
+
+def replaceAfterPathRight(top: BinaryDecisionTree, path: Path, replacement: BinaryDecisionTree): BinaryDecisionTree = top match {
+    case BinaryDecisionTree.Zero | BinaryDecisionTree.One => replacement
+    case BinaryDecisionTree.NonLeaf(id, left, right) =>
+        assert:
+            id == path.head
+
+        path match
+            case Path.Leaf(_) => BinaryDecisionTree.NonLeaf(id, left, replacement)
+            case Path.ConsLeft(_, tail) => BinaryDecisionTree.NonLeaf(id, replaceAfterPathRight(left, tail, replacement), right)
+            case Path.ConsRight(_, tail) => BinaryDecisionTree.NonLeaf(id, left, replaceAfterPathRight(right, tail, replacement))
+}
+
+// returned paths will always end with the duplicate event
+def getRepeatingPath(bdt: BinaryDecisionTree): Option[Path] =
+    getPaths(bdt)
+        .map(path => (path, repeatedVertex(path)))
+        .collectFirst { case (path, Some(duplicateEvent)) if path.last == duplicateEvent => path }
+
+def eliminateOne(bdt: BinaryDecisionTree, path: Path): BinaryDecisionTree = {
+    // v (but it's also equal to u)
+    val duplicateEvent = path.last
+
+    // u...v
+    val pathInBetween = path.from(duplicateEvent)
+
+    if isOnlyLeft(pathInBetween) then
+        // replace v and its left and right branches by the left branch of v
+        val BinaryDecisionTree.NonLeaf(v, left, _) = traverse(bdt, path): @unchecked
+        replaceViaPath(bdt, path, left)
+    else if isOnlyRight(pathInBetween) then
+        // replace v and its left and right branches by the right branch of v
+        val BinaryDecisionTree.NonLeaf(v, _, right) = traverse(bdt, path): @unchecked
+        replaceViaPath(bdt, path, right)
+    else
+        val pathUpToU = path.upTo(duplicateEvent)
+
+        pathInBetween match
+            case Path.ConsRight(u, _) =>
+                // replace the right branch of u by the right branch of v
+                val BinaryDecisionTree.NonLeaf(v, _, right) = traverse(bdt, path): @unchecked
+                replaceAfterPathRight(bdt, pathUpToU, right)
+            case Path.ConsLeft(u, _) =>
+                // replace the left branch of u by the left branch of v
+                val BinaryDecisionTree.NonLeaf(v, left, _) = traverse(bdt, path): @unchecked
+                replaceAfterPathLeft(bdt, pathUpToU, left)
+}
+
+def eliminateRepeatedly(bdt: BinaryDecisionTree): BinaryDecisionTree = {
+    var tree = bdt
+
+    var repeatingPath = getRepeatingPath(tree)
+    while repeatingPath.nonEmpty do
+        tree = eliminateOne(tree, repeatingPath.get)
+        repeatingPath = getRepeatingPath(tree)
+    end while
+
+    tree
 }
