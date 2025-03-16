993af6877525458927b668ee71871bda54b2d1a2
Jan Boerman <JanBoerman95@gmail.com>
2025-03-16 11:57:27 +0100

    Regenerate .dft files again for Storm, based on the original handcreated .dft files.

--- src/main/scala/bdd/Generate.scala
+++ src/main/scala/bdd/Generate.scala
@@ -2,29 +2,13 @@ package bdd
 
 import benchmark.Conversion
 import dft.{DFT, DFTNode}
-import reallife.{ATCFailsToResolveTheConflict, AircraftRunwayExcursionAccidents, AssessingTheRisks1, BHNGPipeline, ChlorineRelease, DagLikeFaultTree, HSC, LeakageFailure, LiquidStorageTankFromDFT, LossContainerAtPort, MainTrackTrainCollisionsLeadingToFatalitiesAndInjuries, OGPF, PCBA, SimpleFaultTree, SubmarinePipelineStopperFailure, T0Chopper, TreeLikeFaultTree}
+import reallife.{ATCFailsToResolveTheConflict, AircraftRunwayExcursionAccidents, AssessingTheRisks1, BHNGPipeline, ChlorineRelease, DagLikeFaultTree, HSC, LeakageFailure, LiquidStorageTankFromDFT, LossContainerAtPort, MainTrackTrainCollisionsLeadingToFatalitiesAndInjuries, OGPF, PCBA, SimpleFaultTree, SubmarinePipelineStopperFailure, T0Chopper, TreeLikeFaultTree, TreesInPaper}
 
 import java.io.File
 
 object Generate {
 
-    val trees: Seq[SimpleFaultTree] = Seq(
-        AircraftRunwayExcursionAccidents,
-        MainTrackTrainCollisionsLeadingToFatalitiesAndInjuries,
-        ATCFailsToResolveTheConflict,
-        LiquidStorageTankFromDFT,
-        LossContainerAtPort,
-        SubmarinePipelineStopperFailure,
-        BHNGPipeline,
-        LeakageFailure,
-        AssessingTheRisks1,
-        PCBA,
-        HSC,
-
-        ChlorineRelease,
-        T0Chopper,
-        OGPF,
-    )
+    val trees: Seq[SimpleFaultTree] = TreesInPaper.faultTrees;
 
     def dftLines(sft: SimpleFaultTree): Seq[DFTNode] = sft match {
         case tlft: TreeLikeFaultTree => Conversion.translateToDFT(tlft.FT)
